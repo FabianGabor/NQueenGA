16e7c6c5a9e6f1462c09d4011edfa626f60f2bcd
Fábián Gábor <info@fabiangabor.com>
2022-04-04 16:13:30 +0200

    config.properties print.population.init

--- src/main/java/hu/nye/mi/nqueen/service/GeneticAlgorithm.java
+++ src/main/java/hu/nye/mi/nqueen/service/GeneticAlgorithm.java
@@ -15,6 +15,9 @@ public class GeneticAlgorithm {
     private final int sameWinnerMaxCount;
     private final Board board;
 
+    @Value("${print.population.init}")
+    private boolean printPopulationInit;
+
     @Value("${print.population}")
     private boolean printPopulation;
 
@@ -25,8 +28,10 @@ public class GeneticAlgorithm {
     private boolean printBoard;
 
     public void solve(View view) {
-        for (Individual individual : population.getIndividuals()) {
-            view.print(String.valueOf(individual));
+        if (printPopulationInit) {
+            for (Individual individual : population.getIndividuals()) {
+                view.print(String.valueOf(individual));
+            }
         }
 
         int generation = 0;
