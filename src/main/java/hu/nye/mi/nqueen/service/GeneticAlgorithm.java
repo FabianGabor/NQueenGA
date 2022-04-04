616dde328c57c53f23194018fd38081ef6d6a34d
Fábián Gábor <info@fabiangabor.com>
2022-04-04 14:31:55 +0200

    add stopping criteria sameWinnerMaxCount

--- src/main/java/hu/nye/mi/nqueen/service/GeneticAlgorithm.java
+++ src/main/java/hu/nye/mi/nqueen/service/GeneticAlgorithm.java
@@ -1,5 +1,6 @@
 package hu.nye.mi.nqueen.service;
 
+import hu.nye.mi.nqueen.domain.Board;
 import hu.nye.mi.nqueen.domain.Individual;
 import hu.nye.mi.nqueen.domain.Population;
 import hu.nye.mi.nqueen.view.View;
@@ -10,6 +11,8 @@ public class GeneticAlgorithm {
     private Population population;
     private double crossoverRate;
     private double mutationRate;
+    int sameWinnerMaxCount;
+    private Board board;
 
     public void solve(View view) {
         for (Individual individual : population.getIndividuals()) {
@@ -17,13 +20,27 @@ public class GeneticAlgorithm {
         }
 
         int generation = 0;
+        int sameWinner = 0;
 
-        while (population.getFittestIndividualFitnessValue() > 0) {
+        while (population.getFittestIndividualFitnessValue() > 0 && sameWinner < sameWinnerMaxCount) {
+            Individual oldFittestIndividual = population.getFittestIndividual();
             population.evolve(crossoverRate, mutationRate);
+            Individual newFittestIndividual = population.getFittestIndividual();
+
+            if (oldFittestIndividual.equals(newFittestIndividual)) {
+                sameWinner++;
+            } else {
+                sameWinner = 0;
+            }
+
             generation++;
-            //view.print(String.valueOf(population));
-            view.print("Winner " + generation + ": " + population.getFittestIndividual());
+            view.print(String.valueOf(population));
+            view.print("Winner " + generation + ": " + newFittestIndividual);
         }
-        //view.print("Solution: " + "Generation " + generation + ": " + population.getFittestIndividual());
+
+
+        board.putQueens(population.getFittestIndividual().getChromosome());
+
+        view.print(String.valueOf(board));
     }
 }
